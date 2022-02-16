8f1b503c4364f966f5a3d9b67f11c0464e02c41c
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-02-16 21:12:08 +0500

    origin

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,13 @@
 
 import Head from 'next/head'
-import '../styles/globals.css'
-import Script from 'next/script'
-import { useEffect } from 'react'
+import '../styles/globals.css' 
 
-function MyApp({ Component, pageProps }) {
-  // useEffect(()=>{
-  //   let script = document.createElement('script')
-  //   script.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js')
-  //   document.querySelector("body").appendChild(script)
-  // },[])
+function MyApp({ Component, pageProps }) {  
   return (
     <>
       <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" />
-        <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/fontawesome.min.css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/regular.min.css" />
-        <link rel='icon' href='/favicon.png' />
+      <link rel='icon' href='/favicon.png' />
+        <meta name="theme-color" content="#2CBC63"/>
         <link rel='manifest' href='/manifest.json' />
       </Head>
 
